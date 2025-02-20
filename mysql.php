6fb09f768f17393b72f370d46a8db0933bb71342
Aditya Aishwarya <144144156+Aditya1286@users.noreply.github.com>
2025-02-20 19:09:48 +0530

    Add files via upload

---
+++ mysql.php
@@ -0,0 +1,26 @@
+<?php
+echo "Welcome to the stage were we are ready to get connected to data base<br>";
+/*Ways to connect to a MySQL Datebase
+1. MySQlLi extension
+two types of Mysqli extension 
+a. object orieneted 
+b. procedural 
+2. PDO-Php Data Object
+*/
+//connecting to a database
+$servername="localhost";
+$username="root";
+$password="";
+//create a connection 
+$conn =mysqli_connect($servername,$username,$password);
+
+//Die if connection is not successfull
+if(!$conn){
+    die("Sorry we failed to connect: ". mysqli_connect_error());
+}
+else{
+    echo "Connection was successfull";
+}
+//close the connection when done;
+mysqli_close($conn);
+?>
